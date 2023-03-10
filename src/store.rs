085e3eb2740bd2f2f9472a8221a8bc4cd1cbe583
nireo <eemil.lehtikangas1@gmail.com>
2023-03-10 23:59:51 +0200

    implement store functions

--- src/store.rs
+++ src/store.rs
@@ -1,6 +1,11 @@
+use std::path::Path;
+
 use anyhow::Result;
 use bytes::Bytes;
 use sled::Db;
+
+const TOPIC_FMT: &str = "t-{}-{}";
+const ACK_TOPIC_FMT: &str = "t-{}-ack-{}";
 
 pub struct Value {
     dacks: usize,
@@ -31,4 +36,39 @@ pub trait Store {
 
 pub struct SledStore {
     db: Db,
+    path: Path,
+}
+
+impl Store for SledStore {
+    fn ack(&self, topic: &str, offset: usize) -> Result<()> {
+        let key = format!("t-{}-ack-{}", topic, offset);
+        self.db.remove(key)?;
+
+        Ok(())
+    }
+
+    fn nack(&self, topic: &str, offset: usize) -> Result<()> {
+        Ok(())
+    }
+
+    fn purge(&self, topic: &str) -> Result<()> {
+        let mut batch = sled::Batch::default();
+        let prefix = format!("t-{}", topic).as_bytes();
+        let mut prefix_iter = self.db.scan_prefix(prefix);
+
+        loop {
+            let entry = prefix_iter.next();
+            if entry.is_none() {
+                break;
+            }
+
+            let entry = entry.unwrap();
+            if entry.is_err() {
+                break;
+            }
+            let entry = entry.unwrap();
+        }
+
+        Ok(())
+    }
 }
